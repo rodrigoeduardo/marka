a51327da6cee50701ca661cc895f81a20bf662e8
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2024-05-03 05:53:25 -0300

    feat: add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center w-full px-8 sm:px-16 py-6 bg-[#EAFDFF] mt-16">
+      © 2024 Marka.
+    </footer>
+  );
+};
