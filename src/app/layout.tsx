0b557f69c16e28da22596d4cc8d2f1472093a7e9
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2024-05-03 05:52:09 -0300

    feat: add PT Sans Narrow font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, PT_Sans_Narrow } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import { cn } from "@/utils/tailwindUtils";
+import { Footer } from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const ptSansNarrow = PT_Sans_Narrow({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-pt-sans-narrow",
+});
 
 export const metadata: Metadata = {
   title: "Marka | Artefatos Escolares",
@@ -17,10 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>
+      <body className={cn(inter.className, ptSansNarrow.className)}>
         <Navbar />
 
-        <main className="px-2">{children}</main>
+        {children}
+
+        <Footer />
       </body>
     </html>
   );
