db38d79bdcffa547b043b52d38d847e7639a0dfa
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2024-05-03 05:53:15 -0300

    feat: add Logo to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,26 @@
+"use client";
+import Image from "next/image";
+
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full px-8 py-6">
-      <h1 className="font-bold text-lg">MARKA</h1>
+    <nav className="flex justify-between items-center w-full px-8 sm:px-16 py-6 bg-[#EAFDFF] drop-shadow-lg">
+      <Image
+        src={"/assets/brand/marka-logo.png"}
+        alt={"Marka Logo"}
+        width={2297}
+        height={835}
+        className="h-auto w-[240px] max-sm:hidden"
+      />
 
-      <ul className="flex gap-x-4">
+      <Image
+        src={"/assets/brand/marka-logo-no-subtitle.png"}
+        alt={"Marka Logo"}
+        width={2297}
+        height={642}
+        className="h-auto w-[120px] sm:hidden"
+      />
+
+      <ul className="flex gap-x-4 text-2xl max-sm:text-lg">
         <li>
           <a href="#produtos">🛒 Produtos</a>
         </li>
