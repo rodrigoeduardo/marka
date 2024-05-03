2b13597f123b5353b05c4c1f7085129b77336bc6
Rodrigo Eduardo <rodrigoeduardodb1@gmail.com>
2024-05-03 17:00:32 -0300

    feat: add more products

--- src/domain/products.ts
+++ src/domain/products.ts
@@ -1,6 +1,6 @@
 export enum Unit {
-  MILILITRO = "ML",
-  LITRO = "LT",
+  MILILITER = "ML",
+  LITER = "LT",
 }
 
 export enum Color {
@@ -17,6 +17,8 @@ export enum ProductType {
 export enum ProductName {
   LIMPADOR_QUADRO_BCO_100ML,
   LIMPADOR_QUADRO_BCO_500ML,
+  LIMPADOR_QUADRO_BCO_1L,
+  LIMPADOR_QUADRO_BCO_5L,
 
   TINTA_MARCADOR_QUADRO_BCO_AZUL_1LT,
   TINTA_MARCADOR_QUADRO_BCO_AZUL_500ML,
@@ -36,6 +38,9 @@ export type Product = {
   unit: Unit;
   serialNumber: number;
   color?: Color;
+  image: {
+    url: string;
+  };
 };
 
 export const PRODUCTS_MAP: Record<ProductName, Product> = {
@@ -43,14 +48,38 @@ export const PRODUCTS_MAP: Record<ProductName, Product> = {
   [ProductName.LIMPADOR_QUADRO_BCO_100ML]: {
     type: ProductType.LIMPADOR_QUADRO_BCO,
     quantity: 100,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 13,
+    image: {
+      url: "/assets/products/13.jpg",
+    },
   },
   [ProductName.LIMPADOR_QUADRO_BCO_500ML]: {
     type: ProductType.LIMPADOR_QUADRO_BCO,
     quantity: 500,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 14,
+    image: {
+      url: "/assets/products/14.jpg",
+    },
+  },
+  [ProductName.LIMPADOR_QUADRO_BCO_1L]: {
+    type: ProductType.LIMPADOR_QUADRO_BCO,
+    quantity: 1,
+    unit: Unit.LITER,
+    serialNumber: 18,
+    image: {
+      url: "/assets/products/18.png",
+    },
+  },
+  [ProductName.LIMPADOR_QUADRO_BCO_5L]: {
+    type: ProductType.LIMPADOR_QUADRO_BCO,
+    quantity: 5,
+    unit: Unit.LITER,
+    serialNumber: 19,
+    image: {
+      url: "/assets/products/19.png",
+    },
   },
 
   // TINTAS
@@ -59,62 +88,86 @@ export const PRODUCTS_MAP: Record<ProductName, Product> = {
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_AZUL_1LT]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 1,
-    unit: Unit.LITRO,
+    unit: Unit.LITER,
     serialNumber: 28,
     color: Color.BLUE,
+    image: {
+      url: "/assets/products/28.jpg",
+    },
   },
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_AZUL_500ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 500,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 16,
     color: Color.BLUE,
+    image: {
+      url: "/assets/products/16.jpg",
+    },
   },
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_AZUL_20ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 20,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 7,
     color: Color.BLUE,
+    image: {
+      url: "/assets/products/7.jpg",
+    },
   },
 
   // -- PRETO
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_PRETO_1LT]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 1,
-    unit: Unit.LITRO,
+    unit: Unit.LITER,
     serialNumber: 27,
     color: Color.BLACK,
+    image: {
+      url: "/assets/products/27.jpg",
+    },
   },
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_PRETO_500ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 500,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 15,
     color: Color.BLACK,
+    image: {
+      url: "/assets/products/15.jpg",
+    },
   },
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_PRETO_20ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 20,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 6,
     color: Color.BLACK,
+    image: {
+      url: "/assets/products/6.jpg",
+    },
   },
 
   // -- VERMELHO
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_VERMELHO_500ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 500,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 17,
     color: Color.RED,
+    image: {
+      url: "/assets/products/17.jpg",
+    },
   },
   [ProductName.TINTA_MARCADOR_QUADRO_BCO_VERMELHO_20ML]: {
     type: ProductType.TINTA_MARCADOR_QUADRO_BCO,
     quantity: 20,
-    unit: Unit.MILILITRO,
+    unit: Unit.MILILITER,
     serialNumber: 8,
     color: Color.RED,
+    image: {
+      url: "/assets/products/8.jpg",
+    },
   },
 };
 
